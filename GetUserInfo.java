a0c538af747d911dfaf3e93bd811fc697b19834c
oluedun <50808885+oluedun@users.noreply.github.com>
2019-08-10 14:33:02 -0700

    Add files via upload

---
+++ GetUserInfo.java
@@ -0,0 +1,36 @@
+
+
+        /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package getuserinfo;
+
+/**
+ *
+ * @author oedun
+ */
+import java.util.Scanner;
+public class GetUserInfo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name;
+        int age;
+        Scanner inputDevice = new Scanner(System.in);
+        System.out.print("Please enter your name >> ");
+        name = inputDevice.nextLine();
+        System.out.print("Please enter your age >> ");
+        age = inputDevice.nextInt();
+        
+        
+        
+        
+        
+        
+    }
+    
+}
